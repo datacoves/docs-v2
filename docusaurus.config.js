7eb9fa51419624fe76e194d6d4ec2c985344ac66
Noel Gomez <noel_gomez@yahoo.com>
2025-09-13 07:10:33 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   url:'https://docs.datacov.es',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-v2',
+  baseUrl: '/docs-v2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
